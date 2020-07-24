dbee7fbe4708b13c6ce4f1ecdffa1f84610670a6
Keith <ko.keith@hotmail.com>
2020-07-24 15:38:24 -0400

    edit articles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_24_192822) do
+ActiveRecord::Schema.define(version: 2020_07_24_193535) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
