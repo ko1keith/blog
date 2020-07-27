e0598805ae5493dcde4931fecd1a42a4da2a42cb
Keith <ko.keith@hotmail.com>
2020-07-27 12:30:03 -0400

    add edit and update action for article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,39 @@
 class ArticlesController < ApplicationController
 
+    #show single article
     def show 
         @article = Article.find(params[:id])
     end
 
+    #display all articles from database
     def index 
         @articles = Article.all
     end
 
+    #display new article page
     def new
         #for first time page loads
         @article = Article.new
     end
 
+    #find article and display the form
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    #take edited form and update to database
+    def update 
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title,:description))
+            flash[:notice] = "Article was updated succesfully."
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+    end
+
+
+    #take form from new article page, create new article and add to database
     def create 
         #white list article params
         #require top level key of article and only permit title and description parameters
@@ -27,6 +48,8 @@ class ArticlesController < ApplicationController
             render 'new'
         end
     end
+
+
     
 
 end
