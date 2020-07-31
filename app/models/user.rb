4a0d9d774af2965f54459434706eb53f7543a185
Keith <ko.keith@hotmail.com>
2020-07-31 14:04:54 -0400

    add bcrypt gem and back end setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,13 @@ class User < ApplicationRecord
                     length:{minimum:3, maximum:25}
     
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    
     validates :email, presence: true,
                     uniqueness:{case_sensitive: false},
                     length:{maximum:105},
                     format:{with:VALID_EMAIL_REGEX}
 
-    
+    #add bcrypt 
+    has_secure_password
 
 end
