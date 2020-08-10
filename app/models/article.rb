7be21c105d67b66846f9ebe9fbc64c3b4c7bfc6c
Keith <ko.keith@hotmail.com>
2020-08-10 14:56:15 -0400

    add many to many association b/w articles and categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,5 +4,7 @@ class Article < ApplicationRecord
     validates :title, presence: true, length:{minimum:6,maximum:100}
     validates :description, presence: true, length:{minimum:10, maximum:1000}
 
+    has_many :article_categories
+    has_many :categories, through: :article_categories
     
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,7 @@ class Category < ApplicationRecord
     validates :name, presence: true, length:{minimum: 5, maximum: 20},
                 uniqueness:{case_sensitive: false}
 
+    has_many :article_categories
+    has_many :articles, through: :article_categories
+    
 end
